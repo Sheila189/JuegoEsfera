e9dad47707cbaca782e155d0498b100d2533d3fd
Sheila189 <20213tn089@utez.edu.mx>
2024-10-03 10:50:25 -0600

    Base del juego

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    public GameObject spawn;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.position = spawn.transform.position;
+            collision.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 3.0f;
+    public Transform camara;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 forward = camara.forward;
+        Vector3 right = camara.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 moveDirection = (forward * moveZ + right * moveX);
+
+        rb.AddForce(moveDirection * moveSpeed, ForceMode.Acceleration);
+
+        rb.AddForce(moveX * moveSpeed, 0, moveZ * moveSpeed, ForceMode.Acceleration);
+    }
+}
